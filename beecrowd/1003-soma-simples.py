bb22d1246bacfe67d7b1ab3ff0844179bc5498d6
Luana Eliza dos Santos <20261ctb0100061@estudantes.ifpr.edu.br>
2026-04-16 20:39:00 +0000

    Aula 10 - 1003: soma simples

---
+++ beecrowd/1003-soma-simples.py
@@ -0,0 +1,21 @@
+'''
+Problema: beecrowd - 1003
+Data: 2026.04.16
+Estudante: Luana Eliza dos Santos
+'''
+# Objetivo: Ler dois valores inteiros, no caso as variáveis A e B, calcular a soma entre elas e atribuir à variável SOMA. Escrever o valor desta variável.
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: conter 2 valores inteiros
+# Processamento: ler A e B e calcular a soma entre eles
+# Saída: imprima a mensagem "SOMA" com as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B
+
+# Leitura das variáveis (observe o enunciado - quantas entradas há?)
+A = int(input())
+B = int(input())
+
+# Cálculo
+SOMA = A + B
+
+# Saída - observe o formato exato no enunciado
+print(f"SOMA = {SOMA}")
