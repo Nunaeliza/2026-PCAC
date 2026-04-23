818491b0b3948ae32d4e52eeacfd6657e1badeae
Luana Eliza dos Santos <20261ctb0100061@estudantes.ifpr.edu.br>
2026-04-23 19:44:39 +0000

    Aula 10 - 1004: produto simples

---
+++ beecrowd/1004-produto-simples.py
@@ -0,0 +1,19 @@
+'''
+Problema: beecrowd - 1004
+Data: 2026.04.23
+Estudante: Luana Eliza dos Santos
+'''
+#Objetivo: ler dois valores inteiros, calcular o produto entre os dois valores e atribuir esta operação à variável PROD. A seguir mostre a variavel PROD com a mensagem corespondente.
+
+# --- ANÁLISE (LIAC) ---
+#Entrada: o arquivo de entrada contém 2 valores inteiros.
+#Processamento: ler A e B e calcular o produto entre esses dois valores.
+# Saída: imprima a mensagem "PROD" e a variável PROD com um espaço em branco antes e depois da igualdade.
+
+A = int(input())
+B = int(input())
+
+# Qual operação o enunciado pede?
+PROD = A * B
+# Saída - observe o formato exato no enunciado
+print(f"PROD = {PROD}")
