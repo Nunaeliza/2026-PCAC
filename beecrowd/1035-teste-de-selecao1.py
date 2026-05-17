fcd9a4e3714a92549b4095defd15edf6a7d7984f
Luana Eliza dos Santos <20261ctb0100061@estudantes.ifpr.edu.br>
2026-05-17 22:58:58 +0000

    Aula 14 - 1021: notas e moedas

---
+++ beecrowd/1035-teste-de-selecao1.py
@@ -0,0 +1,11 @@
+'''
+Problema: beecrowd - 1035
+Data: 2026.05.17
+Estudante: Luana Eliza dos Santos
+'''
+# Objetivo: ler quatro valores inteiros A, B, C e D
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: 
+# Processamento:
+# Saída: 
